c56222a1d571e4ce8103fa644d72cda07fd392c1
Dan Foley <dan@dan-foley.com>
2019-12-16 22:49:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -122,6 +122,6 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   cart_with_coupons_applied = apply_coupons(consolidated_cart, coupons)
   cart_with_clearance_applied = apply_clearance(cart_with_coupons_applied)
-  # binding.pry
+  binding.pry
 
 end
